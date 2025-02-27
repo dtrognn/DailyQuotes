ae620b7fb24095cb6702c74477158dda8dae4a50
dtrognn <dtrognn@gmail.com>
2025-02-27 13:43:22 +0700

    init project

---
+++ DailyQuotes/DailyQuotes/DailyQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuotesApp.swift
+//  DailyQuotes
+//
+//  Created by dtrognn on 27/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
