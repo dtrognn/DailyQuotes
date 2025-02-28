f090deb2616267b7c2a133fb7b55ed568e0eec00
dtrognn <dtrognn@gmail.com>
2025-02-28 11:07:28 +0700

    chore(common): change public layout size

--- DailyQuotes/DevPods/DQCommon/DQCommon/AppStyle/Layout.swift
+++ DailyQuotes/DevPods/DQCommon/DQCommon/AppStyle/Layout.swift
@@ -7,37 +7,37 @@
 
 import SwiftUI
 
-struct LayoutSize {
+public enum LayoutSize {
     // Padding
-    static let smallPadding: CGFloat = 8.0
-    static let mediumPadding: CGFloat = 16.0
-    static let largePadding: CGFloat = 24.0
+    public static let smallPadding: CGFloat = 8.0
+    public static let mediumPadding: CGFloat = 16.0
+    public static let largePadding: CGFloat = 24.0
 
     // Corner Radius
-    static let smallRadius: CGFloat = 4.0
-    static let mediumRadius: CGFloat = 8.0
-    static let largeRadius: CGFloat = 12.0
+    public static let smallRadius: CGFloat = 4.0
+    public static let mediumRadius: CGFloat = 8.0
+    public static let largeRadius: CGFloat = 12.0
 
     // Button Sizes
-    static let buttonHeightSmall: CGFloat = 32.0
-    static let buttonHeightMedium: CGFloat = 44.0
-    static let buttonHeightLarge: CGFloat = 56.0
-    static let buttonMinWidth: CGFloat = 100.0
+    public static let buttonHeightSmall: CGFloat = 32.0
+    public static let buttonHeightMedium: CGFloat = 44.0
+    public static let buttonHeightLarge: CGFloat = 56.0
+    public static let buttonMinWidth: CGFloat = 100.0
 
     // Spacing
-    static let zeroSpacing: CGFloat = 0.0
-    static let smallSpacing: CGFloat = 4.0
-    static let mediumSpacing: CGFloat = 8.0
-    static let largeSpacing: CGFloat = 16.0
+    public static let zeroSpacing: CGFloat = 0.0
+    public static let smallSpacing: CGFloat = 4.0
+    public static let mediumSpacing: CGFloat = 8.0
+    public static let largeSpacing: CGFloat = 16.0
 
     // Icon Sizes
-    static let smallIcon: CGFloat = 16.0
-    static let mediumIcon: CGFloat = 24.0
-    static let largeIcon: CGFloat = 32.0
+    public static let smallIcon: CGFloat = 16.0
+    public static let mediumIcon: CGFloat = 24.0
+    public static let largeIcon: CGFloat = 32.0
 
     // Card Sizes
-    static let cardMinWidth: CGFloat = 200.0
-    static let cardMinHeight: CGFloat = 150.0
+    public static let cardMinWidth: CGFloat = 200.0
+    public static let cardMinHeight: CGFloat = 150.0
 
     // Screen-based calculations
     static func adaptiveWidth(percentage: CGFloat) -> CGFloat {
